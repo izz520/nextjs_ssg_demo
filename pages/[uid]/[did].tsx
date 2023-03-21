e031cb2064f3cc0024183f3dc95ed7354e255905
izz520 <1547471276@qq.com>
2023-03-21 10:51:18 +0800

    finish SSG and ISR

---
+++ pages/[uid]/[did].tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const Detail = ({ time }: { time: any }) => {
+    return (
+        <div>{time}</div>
+    )
+}
+
+export const getStaticPaths = async () => {
+    // let list = [
+    //     {
+    //         uid: '1',
+    //         did: '1'
+    //     },
+    //     {
+    //         uid: '1',
+    //         did: '2'
+    //     }
+    // ]
+    const res = await axios.get("https://ssgdemo.usemock.com/paths");
+    // console.log(res.data.data);
+    const list = res.data.data;
+    const paths = list.map((item: { uid: any; did: any; }) => ({
+        params: {
+            uid: item.uid,
+            did: item.did
+        },
+    }))
+    console.log(paths);
+    return {
+        paths,
+        // fallback: true
+        fallback: "blocking"
+        // fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params }: { params: { uid: string, did: string } }) => {
+    console.log(params);
+    const res = await axios.get(`https://ssgdemo.usemock.com/${params.uid}/${params.did}`);
+    console.log(res.data.data.time);
+    return {
+        props: {
+            time: res.data.data.time
+        },
+        revalidate: 60
+    }
+}
+
+export default Detail;
